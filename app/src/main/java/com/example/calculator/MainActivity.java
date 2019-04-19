86ac1f53007560c2bb31339998e0ef20f35e8dc8
Chris <45054064+StaticallyHyped@users.noreply.github.com>
2019-04-19 13:23:33 -0700

    Added a landscape view layout, nearly finished with the UI, finished functionality.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,12 +2,142 @@ package com.example.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText result;
+    private EditText newNumber;
+    private TextView displayOperation;
+
+    //variables to hold the operands and type of calculations
+
+    private Double operand1 = null;
+    private Double operand2 = null;
+    private String pendingOperation = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.result);
+        newNumber = findViewById(R.id.newNumber);
+        displayOperation = findViewById(R.id.operation);
+
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+
+        Button buttonDecimal = findViewById(R.id.buttonDecimal);
+        Button buttonEquals = findViewById(R.id.buttonEquals);
+        Button buttonDivide = findViewById(R.id.buttonDivide);
+        Button buttonMultiply = findViewById(R.id.buttonMultiply);
+        Button buttonMinus = findViewById(R.id.buttonMinus);
+        Button buttonPlus = findViewById(R.id.buttonPlus);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //must cast to Button because onClickListener is actually a View object type
+                Button b = (Button) v;
+
+                //appends the text of the EditText newNumber with a button instance,
+                //specifically the one that was pressed
+                newNumber.append(b.getText().toString());
+
+            }
+        };
+
+        //make all the buttons implement the oncliclistener that we've built
+        button0.setOnClickListener(listener);
+        button1.setOnClickListener(listener);
+        button2.setOnClickListener(listener);
+        button3.setOnClickListener(listener);
+        button4.setOnClickListener(listener);
+        button5.setOnClickListener(listener);
+        button6.setOnClickListener(listener);
+        button7.setOnClickListener(listener);
+        button8.setOnClickListener(listener);
+        button9.setOnClickListener(listener);
+        buttonDecimal.setOnClickListener(listener);
+
+
+        View.OnClickListener opListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //must cast to Button because onClickListener is actually a View object type
+                Button b = (Button) v;
+                String op = b.getText().toString();
+                String value = newNumber.getText().toString();
+
+                try {
+                    Double doubleValue = Double.valueOf(value);
+                    performOperation(doubleValue, op);
+
+                } catch (NumberFormatException e){
+                newNumber.setText("");
+            }
+
+                pendingOperation = op;
+                displayOperation.setText(pendingOperation);
+
+            }
+        };
+
+        buttonEquals.setOnClickListener(opListener);
+        buttonDivide.setOnClickListener(opListener);
+        buttonMinus.setOnClickListener(opListener);
+        buttonPlus.setOnClickListener(opListener);
+        buttonMultiply.setOnClickListener(opListener);
+    }
+
+    private void performOperation (Double value, String operation){
+        if  ( null == operand1){
+            operand1 =  value;
+
+        }
+        else {
+            operand2 = value;
+            if(pendingOperation.equals("=")){
+                pendingOperation = operation;
+            }
+            switch (pendingOperation){
+                case "=":
+                    operand1 = operand2;
+                    break;
+                case "/":
+                    if(operand2 == 0){
+                        operand1 = 0.0;
+                    }
+                    else {
+                        operand1 /= operand2;
+                    }
+                    break;
+
+                case "*":
+                    operand1 *= operand2;
+                    break;
+                case "-":
+                    operand1 -= operand2;
+                    break;
+                case "+":
+                    operand1 += operand2;
+                    break;
+            }
+
+        }
+        result.setText(operand1.toString());
+        newNumber.setText("");
     }
 }
